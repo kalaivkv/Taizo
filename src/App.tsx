342934192f42ffa5f4e9b734d45a7b85e833f2d5
kalaivanan <kalaivkv24@gmail.com>
2025-03-19 16:53:43 +0530

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+    
     </div>
   );
 };
